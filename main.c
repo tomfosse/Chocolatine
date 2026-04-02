b3e3475b13edd27ce5276dc34759908b68383e0a
Tom Fossé <tom.fosse@epitech.eu>
2026-04-02 10:50:06 +0200

    fix the executables list

--- main.c
+++ main.c
@@ -1,11 +1,9 @@
-/*
-** EPITECH PROJECT, 2026
-** test
-** File description:
-** test
-*/
-
 int main(void)
 {
+    int i = 0;
+    if (i == 0)
+        for (int y = 0; y < 3; y++)
+            if (y == i)
+                return 0;
     return 0;
 }
